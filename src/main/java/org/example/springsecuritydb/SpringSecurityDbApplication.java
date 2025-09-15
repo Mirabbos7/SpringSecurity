7cf057a19ea9ee87b9307bc4b1e40d14f63d2075
Mirabbos7 <mirabbosahmedov7777@gmail.com>
2025-09-15 22:57:01 +0500

    Done

--- src/main/java/org/example/springsecuritydb/SpringSecurityDbApplication.java
+++ src/main/java/org/example/springsecuritydb/SpringSecurityDbApplication.java
@@ -1,7 +1,11 @@
 package org.example.springsecuritydb;
 
+import org.example.springsecuritydb.entity.Role;
+import org.example.springsecuritydb.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,4 +16,16 @@ public class SpringSecurityDbApplication {
         SpringApplication.run(SpringSecurityDbApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findRoleByName("USER").isEmpty()) {
+                roleRepository.save(
+                        Role.builder()
+                                .name("USER")
+                                .build()
+                );
+            }
+        };
+    }
 }
